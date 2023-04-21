e5b365bb35b68cdbf958477c50e4d37bb0530731
Toussaint <Toussaint.Salabanzi@placesforpeople.co.uk>
2023-04-21 15:00:16 +0100

    finished home page

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+// let text = document.querySelector("#textop")
+// text.innerHTML = "HELLO";
+console.log("Toussaint Salabanzi")
+const age = 9;
+function checkAge(age){
+    if(age >= 18 ){
+        console.log("You can Enter")
+    }
+    else{
+        console.log("You can't enter")
+    }
+}
+checkAge(age)
